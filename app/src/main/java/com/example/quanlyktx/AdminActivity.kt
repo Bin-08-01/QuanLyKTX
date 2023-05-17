0e51e6c9f0db057a2abecfad62ad579c6cd4a3a0
Bin-08-01 <minhtri192035@gmail.com>
2023-05-17 22:43:47 +0700

    Update

--- app/src/main/java/com/example/quanlyktx/AdminActivity.kt
+++ app/src/main/java/com/example/quanlyktx/AdminActivity.kt
@@ -4,6 +4,11 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.quanlyktx.databinding.ActivityAdminBinding
+import com.example.quanlyktx.model.BillModel
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
 
 class AdminActivity : AppCompatActivity() {
     private lateinit var binding: ActivityAdminBinding
@@ -18,6 +23,31 @@ class AdminActivity : AppCompatActivity() {
         binding.btnBuildAdmin.setOnClickListener {
 
         }
-        binding.btnUserAdmin.setOnClickListener {  }
+        binding.btnUserAdmin.setOnClickListener { }
+        getData()
+    }
+
+    private fun getData() {
+        FirebaseDatabase.getInstance().getReference("Bills").addValueEventListener(object :
+            ValueEventListener {
+            override fun onDataChange(snapshot: DataSnapshot) {
+                var i = 0
+                if(snapshot.exists()){
+                    for(child in snapshot.children){
+                        val billData = child.getValue(BillModel::class.java)
+                        if(billData?.status == "No"){
+                            i++
+                        }
+                    }
+                    binding.totalBill.text = "Đang có $i đơn cần xét duyệt"
+                }else{
+                    binding.totalBill.text = "Đang có 0 đơn cần xét duyệt"
+                }
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+                binding.totalBill.text = "Đang có 0 đơn cần xét duyệt"
+            }
+        })
     }
 }
